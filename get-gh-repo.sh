1188877cf840bf26a96c635a15879c99b5fb80d7
David Murrell <dmurrell@faulkner.edu>
2017-09-25 17:28:40 -0500

    Initial add of 2 scripts

---
+++ get-gh-repo.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# github api reference
+# https://developer.github.com/v3/
+
+# Written By: Brian Konzman Significantly simplied by David Murrell for class
+
+
+if [ $# -ne 2 ];
+	then
+	echo ""
+	echo "This script will clone a simgle repository from an organization and "
+	echo "  checkout the branch mentioned in the second parameter."
+	echo "Please provide 2 parameters in this order:"
+	echo "1. URL of the repository to be cloned"
+	echo "2. The name of the branch to be checked out"
+else
+
+	url=$1
+	brnch=$2
+	
+	# Make directory sibling to the one of this script.
+	# Make it the current directory.
+	
+	mkdir -p ../work
+	cd ../work
+	
+	# Clone the repository mentioned in the first parameter
+	#   into the work directory and checkout the branch mentioned
+	#   in the second parameter, retoring current directory.
+
+	#  git clone ${url}
+	#  git checkout $brnch
+	cd ../
+	
+	echo "url:    $url"
+	echo "branch: $brnch"
+fi
---
+++ restore-gh-repo.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+# github api reference
+# https://developer.github.com/v3/
+
+# Written By: Brian Konzman Significantly simplied by David Murrell for class
+
+# This script in this file will add, commit and push all repositories in the
+# sibling directory of the one containing this script that is named work.
+# It requires the name of the branch to be restore and a commit message
+
+if [[ $# -ne 2 ]];
+	then
+	echo "This script requires 2 parameter."
+	echo "1. The name of the branch to be restored"
+	echo "2. The commit message"
+else
+	branch=$1
+	commitmsg=$2
+	for f in ../work/*
+		do
+			cd $f
+			# git add -A
+			# git commit -m $commitmsg
+			# git push origin $branch
+			echo "repository: $f"
+			echo "branch    : $1"
+			echo "commit msg: $2"
+			cd ..
+		done
+fi
